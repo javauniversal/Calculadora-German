d71132fdf6ca8e1980443e2b831a9a758361ccb4
German Garcia <ggarcia@intergrupo.com>
2020-05-16 17:26:51 -0500

    la aplicacion sumando correctamente

--- app/src/main/java/com/fiiss/calculadora/MainActivity.java
+++ app/src/main/java/com/fiiss/calculadora/MainActivity.java
@@ -3,12 +3,41 @@ package com.fiiss.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editNumeroUno;
+    private EditText editNumeroDos;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCalcular = findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(this);
+
+        editNumeroUno = findViewById(R.id.editNumeroUno);
+        editNumeroDos = findViewById(R.id.editNumeroDos);
+        txtResultado = findViewById(R.id.txtResultado);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnCalcular) {
+            if (editNumeroUno.getText().toString().trim().isEmpty() || editNumeroDos.getText().toString().trim().isEmpty()) {
+                Toast.makeText(this, "Campos requeridos", Toast.LENGTH_LONG).show();
+            } else {
+                int resultado = Integer.parseInt(editNumeroUno.getText().toString().trim()) + Integer.parseInt(editNumeroDos.getText().toString().trim());
+                txtResultado.setText(String.format("Resultado: %s", resultado));
+            }
+        }
     }
 }
